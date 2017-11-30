f1b8565f7a2f817b9ad0562037f57749e3b0df18
OnlyReFLeX <reflexonly@gmail.com>
2017-11-30 19:36:24 +0300

    presence password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         validates :username, presence: true
-
-
-
-
-
+         validates :username, :password, presence: true
 end

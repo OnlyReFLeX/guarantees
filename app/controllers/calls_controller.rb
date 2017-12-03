a007a3f2f3e1bf9702afad78a9e7e5092d0348b3
OnlyReFLeX <reflexonly@gmail.com>
2017-12-03 21:44:12 +0300

    add period date (created_at, call_date)

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -3,7 +3,11 @@ class CallsController < ApplicationController
   helper_method :sort_column, :sort_direction
 
   def index
-    @calls = Call.search(params[:search]).order(sort_column + " " + sort_direction).paginate(:per_page => 10, :page => params[:page])
+    @calls = Call.date_period(params[:call_date], 'call_date')
+                 .date_period(params[:created_at], 'created_at')
+                 .search(params[:search])
+                 .order(sort_column + " " + sort_direction)
+                 .paginate(:per_page => 10, :page => params[:page])
   end
 
   def show
--- app/models/call.rb
+++ app/models/call.rb
@@ -9,4 +9,28 @@ class Call < ApplicationRecord
       self.where(nil)
     end
   end
+
+  def self.date_period(date, column)
+    if date =~ /\A\d{4}(-)\d{2}(-)\d{2}\s(-)\s\d{4}(-)\d{2}(-)\d{2}$/ && date != ""
+      date = date.split(%r{\s\S\s})
+      if parsed_date(date[0]) && parsed_date(date[1])
+        where("#{column} BETWEEN ? AND ?", date[0], date[1])
+      else
+        self.where(nil)
+      end
+    else
+      self.where(nil)
+    end
+  end
+
+  private
+
+  def self.parsed_date(date)
+    date = Date.parse(date)
+    if date > Date.parse('0001-01-01') && date < Date.parse('9999-12-31')
+      date
+    end
+  rescue ArgumentError, TypeError
+    return false
+  end
 end

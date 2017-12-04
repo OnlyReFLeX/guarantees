b8ac9cc9f0d9d2506ea6e9a470d384d60a7b5909
OnlyReFLeX <reflexonly@gmail.com>
2017-12-04 19:52:57 +0300

    fix date period for call_date & created_at

--- app/models/call.rb
+++ app/models/call.rb
@@ -4,17 +4,20 @@ class Call < ApplicationRecord
 
   def self.search(search)
     if search
-      where('username ILIKE ? OR phone ILIKE ? OR boiler ILIKE ? OR adress ILIKE ? OR master ILIKE ? OR error ILIKE ?', "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
+      where('username ILIKE ? OR phone ILIKE ? OR boiler ILIKE ? OR adress ILIKE ? OR master ILIKE ? OR error ILIKE ?',
+        "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
     else
       self.where(nil)
     end
   end
 
   def self.date_period(date, column)
-    if date =~ /\A\d{4}(-)\d{2}(-)\d{2}\s(-)\s\d{4}(-)\d{2}(-)\d{2}$/ && date != ""
+    if date =~ /\A\d{4}(-)\d{2}(-)\d{2}\s(-)\s\d{4}(-)\d{2}(-)\d{2}$/ && date != "" # regex: 9999-99-99 - 9999-99-99
       date = date.split(%r{\s\S\s})
       if parsed_date(date[0]) && parsed_date(date[1])
-        where("#{column} BETWEEN ? AND ?", Date.parse(date[0]).prev_day, date[1])
+        date[0] = DateTime.parse(date[0]).prev_day.change({hour: 21})
+        date[1] = DateTime.parse(date[1]).change({hour: 20, min: 59, sec: 59})
+        where("#{column} BETWEEN ? AND ?", date[0], date[1])
       else
         self.where(nil)
       end

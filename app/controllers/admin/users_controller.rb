c4c8c44b735b3887547cf25ac6d7e207742e26d8
OnlyReFLeX <reflexonly@gmail.com>
2017-12-15 02:02:13 +0300

    add flash messages

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -8,7 +8,7 @@ class Admin::UsersController < Admin::AdminController
   end
 
   def update
-    if User.where(admin: true).size == 1 and user_params[:admin] == "0"
+    if User.where(admin: true).size <= 1 and user_params[:admin] == "0"
       redirect_to admin_users_path, alert: 'Вы последний администратор.'
     else
       @user.update(user_params)
@@ -19,7 +19,7 @@ class Admin::UsersController < Admin::AdminController
   def destroy
     if User.all.size == 1
       redirect_to admin_users_path, alert: 'Последний пользователь не может быть удален.'
-    elsif User.where(admin: true).size == 1 and @user.admin? == true
+    elsif User.where(admin: true).size <= 1 and @user.admin? == true
       redirect_to admin_users_path, alert: 'Последний администратор не может быть удален.'
     else
       @user.destroy

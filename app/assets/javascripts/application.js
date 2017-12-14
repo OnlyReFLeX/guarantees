003d1acc818888d062c45bb1c75d26ab4b8e31b3
OnlyReFLeX <reflexonly@gmail.com>
2017-12-14 22:02:18 +0300

    add autocomplete adress

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,3 +21,19 @@
 //= require popper
 //= require moment
 //= require bootstrap-daterangepicker
+
+function geolocate() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var geolocation = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      var circle = new google.maps.Circle({
+        center: geolocation,
+        radius: position.coords.accuracy
+      });
+      autocomplete.setBounds(circle.getBounds());
+    });
+  }
+}

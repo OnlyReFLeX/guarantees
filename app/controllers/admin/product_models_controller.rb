3592228920a412842f853477d5ddd69c7c435c13
OnlyReFLeX <reflexonly@gmail.com>
2017-12-31 00:02:28 +0300

    add dynamic forms and table

--- app/controllers/admin/product_models_controller.rb
+++ app/controllers/admin/product_models_controller.rb
@@ -1,15 +1,21 @@
-class ProductModelsController < Admin::AdminController
+class Admin::ProductModelsController < Admin::AdminController
+  before_action :find_id, only: :destroy
   def create
     @product_model = ProductModel.new(product_models_params)
     if @product_model.save
       redirect_to new_admin_boiler_path
-    else
-      render action: 'new'
     end
+  end
+  def destroy
+    @product_model.destroy
+    redirect_to new_admin_boiler_path
   end
 
   private
   def product_models_params
-    params.require(:product_model).permit(:name)
+    params.require(:product_model).permit(:name, :boiler_id)
+  end
+  def find_id
+    @product_model = ProductModel.find(params[:id])
   end
 end

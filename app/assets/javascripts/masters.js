9a94c124875480ad371bf078c355a17f8179599b
OnlyReFLeX <reflexonly@gmail.com>
2018-01-03 18:08:09 +0300

    add render form update for master

--- app/assets/javascripts/masters.js
+++ app/assets/javascripts/masters.js
@@ -1,23 +1,23 @@
-$(document).on("turbolinks:load", function() {
-  $('a.edit_master').click(function(e) {
-    e.preventDefault();
-    var master_id;
-    var form;
-    var title;
+$(document).on( "click", "a.edit_master", "turbolinks:load", function(e) {
+  e.preventDefault();
+  var master_id;
+  var form;
+  var title;
+  var danger_form;
+  master_id = $(this).data('masterId')
 
-    master_id = $(this).data('masterId')
+  danger_form = $('.master-errors_' + master_id);
+  form = $('#edit_master_' + master_id);
+  title = $('#master_title_' + master_id);
 
-    form = $('#edit_master_' + master_id);
-    title = $('#master_title_' + master_id);
-
-    if (!$(this).hasClass('cancel')) {
-      $(this).removeClass('fa-pencil-square-o');
-      $(this).addClass('fa-times cancel');
-    } else {
-      $(this).removeClass('fa-times cancel');
-      $(this).addClass('fa-pencil-square-o');
-    }
-    form.toggle();
-    title.toggle();
-  });
+  if (!$(this).hasClass('cancel')) {
+    $(this).removeClass('fa-pencil-square-o');
+    $(this).addClass('fa-times cancel');
+  } else {
+    $(this).removeClass('fa-times cancel');
+    $(this).addClass('fa-pencil-square-o');
+  }
+  form.toggle();
+  title.toggle();
+  danger_form.toggle();
 });

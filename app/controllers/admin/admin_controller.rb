bba8c759e6620733244110fce3929261ca894c9f
OnlyReFLeX <reflexonly@gmail.com>
2018-01-19 23:17:44 +0300

    fix name

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,9 +1,9 @@
 class Admin::AdminController < ApplicationController
 
-  before_action :cheack_admin
+  before_action :check_admin
 
   protected
-  def cheack_admin
+  def check_admin
     redirect_to root_path, alert: "У вас недостаточно прав !" unless current_user.admin?
   end
 end
